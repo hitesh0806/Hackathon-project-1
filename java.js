0bdc473a47c53ad0c2a9d591b22eb00385de57da
hitesh0806 <hiteshtg812@gmail.com>
2025-05-16 11:34:29 +0530

    Created a online reporting website

---
+++ java.js
@@ -0,0 +1,33 @@
+function robbery()
+{
+   window.location.href="robbery.html" 
+}
+
+function murder()
+{
+   window.location.href="murder.html" 
+}
+function missing()
+{
+    window.location.href="missing.html"
+}
+function harassment()
+{
+   window.location.href="harrasment.html" 
+}
+function self3()
+{
+    window.location.href="harrasmentsrp.html"
+}
+function thirdparty3()
+{
+    window.location.href="harrasment3rp.html"
+}
+function corruption()
+{
+    window.location.href="corruption.html"
+}
+function substanceabuse()
+{
+    window.location.href="substance abuse.html"
+}
